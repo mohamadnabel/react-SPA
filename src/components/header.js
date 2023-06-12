5e6346fcc9d1abe398bd867d379ca13d88de2267
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2023-06-12 21:49:41 +0300

    update

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+function header() {
+  return (
+    <div className="navbar-paragraph">
+      <p>I'm the paragraph navbar</p>
+      <h1>well i hope that</h1>
+    </div>
+  );
+}
+
+export default header;
